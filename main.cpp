ba770157a71b457272849744e3e675faba95f2fd
sachinthakur13 <35323597+sachinthakur13@users.noreply.github.com>
2018-01-16 08:20:51 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=0,b=1,c=1,d=0;
+    for(int i=0;i<=3;i++)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+
+
+        if (c%2==0)
+        {
+            d+=d;
+            cout<<c;
+        }
+    }
+    cout<<endl;
+
+    return 0;
+}
